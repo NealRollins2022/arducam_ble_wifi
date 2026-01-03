4f2df28b433323d54047aeb36d72ec688daf9ba2
NealRollins2022 <110839717+NealRollins2022@users.noreply.github.com>
2026-01-03 05:28:35 -0500

    Change arducam mega config to arducam nord config

--- drivers/video/arducam_nord/arducam_nord.c
+++ drivers/video/arducam_nord/arducam_nord.c
@@ -142,8 +142,8 @@ static const struct video_driver_api arducam_mega_driver_api = {
 
 /* Device instance declaration */
 static struct arducam_mega_data arducam_mega_data_0;
-static const struct arducam_mega_config arducam_mega_config_0 = {
-    .bus = SPI_DT_SPEC_GET(DT_NODELABEL(arducam0), SPI_OP_MODE_MASTER | SPI_TRANSFER_MSB),
+static const struct arducam_nord_config arducam_nord_config_0 = {
+    .bus = SPI_DT_SPEC_GET(DT_NODELABEL(spi0), SPI_OP_MODE_MASTER | SPI_WORD_SET(8) | SPI_TRANSFER_MSB, 0),
 };
 
 DEVICE_DT_DEFINE(DT_NODELABEL(arducam0),
